4f4d70eea6bb379a5038f4de95c91ed37ee389d0
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-12 19:54:36 -0500

    Grabbed DOM elements

---
+++ app.js
@@ -0,0 +1,7 @@
+const wallDropdown = document.getElementById('wall-dropdown');
+const paintingDropdown = document.getElementById('painting-dropdown');
+const frameDropdown = document.getElementById('frame-dropdown');
+
+const wallImage = document.getElementById('wall-image');
+const paintingImage = document.getElementById('painting-image');
+const frameImage = document.getElementById('frame-image');
