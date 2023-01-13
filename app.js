25ac89d2224fca3c285126855d4d645976d5d0eb
ALEJANDRO RAFAEL LOPEZ <alejandroxrxlopez@gmail.com>
2023-01-12 20:32:06 -0500

    console.log(click)

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ const wallImage = document.getElementById('wall-image');
 const paintingImage = document.getElementById('painting-image');
 const frameImage = document.getElementById('frame-image');
 
+const phraseInput = document.getElementById('phrase-input');
+const phraseButton = document.getElementById('phrase-button');
+
 wallDropdown.addEventListener('change', () => {
     wallImage.src = `./assets/${wallDropdown.value}-wall.png`;
+});
+
+paintingDropdown.addEventListener('change', () => {
+    paintingImage.src = `./assets/${paintingDropdown.value}-painting.png`;
+});
+
+frameDropdown.addEventListener('change', () => {
+    frameImage.src = `./assets/${frameDropdown.value}-frame.png`;
+});
+
+phraseButton.addEventListener('click', () => {
+    console.log('click');
 });
